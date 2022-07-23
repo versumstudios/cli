a706355df039973691b91f0917a1a02dbff09220
d-pettersson <david.pettersson@code.berlin>
2022-07-23 10:55:04 +0200

    feat(export): add export token

---
+++ src/utils/csv.ts
@@ -0,0 +1,9 @@
+import ObjectsToCsv from 'objects-to-csv';
+
+import { log } from './logger';
+
+export const SaveToFile = async (filename: string, data: object[]) => {
+  const csv = new ObjectsToCsv(data);
+  await csv.toDisk(`./${filename}`);
+  log('file saved:', filename);
+};
