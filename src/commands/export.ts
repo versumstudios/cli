b17046f2ae85834db934c7976adf4fb9875df3df
d-pettersson <david.pettersson@code.berlin>
2022-07-24 21:49:35 +0200

    feat(export): wallet-history

--- src/commands/export.ts
+++ src/commands/export.ts
@@ -2,6 +2,7 @@ import { Command } from 'commander';
 
 import { action as exportCollectorsAction } from '@actions/export-collectors';
 import { action as exportTokenCollectorsAction } from '@actions/export-token-collectors';
+import { action as exportWalletHistory } from '@actions/export-wallet-history';
 
 export const command = new Command('export').description('export token data to csv');
 // usage: versum export token-collectors
@@ -14,3 +15,5 @@ command
   .action((option) => {
     exportCollectorsAction(option.platform);
   });
+//usage: versum export wallet-history
+command.command('wallet-history').description('exports wallet history').action(exportWalletHistory);
